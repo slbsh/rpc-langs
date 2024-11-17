cac053c12c1cd390d6b6ce9d6f31369610a072e3
slbsh <crafter.antek@gmail.com>
2024-11-17 17:06:57 +0100

    fix: spaces to tabs

--- thing.c
+++ thing.c
@@ -6,59 +6,59 @@
 
 // thanks to fami_fish
 char** split(char* str, char* delim) {
-    char** words = malloc(sizeof(char*) * strlen(str));
+	char** words = malloc(sizeof(char*) * strlen(str));
 
-    int i = 0;
-    words[i] = strtok(str, delim);
+	int i = 0;
+	words[i] = strtok(str, delim);
 
-    while (words[i])
-        words[++i] = strtok(NULL, delim);
+	while (words[i])
+		words[++i] = strtok(NULL, delim);
 
-    return words;
+	return words;
 }
 
 int calc_line(char* line) {
-    char** words = split(line, " ");
+	char** words = split(line, " ");
 
-    if (words[0] == 0 || words[1] == 0) {
-        puts("Invalid input");
-        free(words);
-        return 0;
-    }
+	if (words[0] == 0 || words[1] == 0) {
+		puts("Invalid input");
+		free(words);
+		return 0;
+	}
 
-    int acc = atoi(words[1]);
+	int acc = atoi(words[1]);
 
-    for (int i = 2; words[i] != 0; i++) {
-        switch (words[0][0]) {
-            case '+':
-                acc += atoi(words[i]);
-                break;
-            case '-':
-                acc -= atoi(words[i]);
-                break;
-            case '*':
-                acc *= atoi(words[i]);
-                break;
-            case '/':
-                acc /= atoi(words[i]);
-                break;
-            default:
-                puts("Invalid operator");
-                free(words);
-                return 0;
-        }
-    }
+	for (int i = 2; words[i] != 0; i++) {
+		switch (words[0][0]) {
+			case '+':
+				acc += atoi(words[i]);
+				break;
+			case '-':
+				acc -= atoi(words[i]);
+				break;
+			case '*':
+				acc *= atoi(words[i]);
+				break;
+			case '/':
+				acc /= atoi(words[i]);
+				break;
+			default:
+				puts("Invalid operator");
+				free(words);
+				return 0;
+		}
+	}
 
-    free(words);
-    return acc;
+	free(words);
+	return acc;
 }
 
 int main() {
-    char input[100];
+	char input[100];
 
-    while (1) {
-        if (!fgets(input, sizeof(input), stdin)) break;
-        printf("%d\n", calc_line(input));
-        memset(input, 0, sizeof(input));
-    }
+	while (1) {
+		if (!fgets(input, sizeof(input), stdin)) break;
+		printf("%d\n", calc_line(input));
+		memset(input, 0, sizeof(input));
+	}
 }
